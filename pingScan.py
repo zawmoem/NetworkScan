8c999b1629b1803e2e41ab7e1a612259564b7a55
zawmoem <154986268+zawmoem@users.noreply.github.com>
2023-12-29 16:19:10 +0800

    Update pingScan.py

--- pingScan.py
+++ pingScan.py
@@ -10,49 +10,59 @@ import subprocess
 import ipaddress
 import json
 import concurrent.futures
-
+import os
+import platform
 import sys
 
 active_hosts= {"active_ip_addrs": []}
 inactive_hosts= {"inactive_ip_addrs": []}
 
-def pingda(ip_addr):
+def pingsweep(cmdPing, ip_addr):
+	
+	#Run ping command with IP address
+	response = os.popen(cmdPing + " " + ip_addr)
 
-#Windows 
-#Ref https://stackoverflow.com/questions/35750041/check-if-ping-was-successful-using-subprocess-in-python
-#
-	canPing = subprocess.call('ping -n 1 -w 500 %s' % ip_addr,stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
-	if canPing == 0:
-		active_hosts["active_ip_addrs"].append(ip_addr)
-	if canPing == 1:
-		inactive_hosts["inactive_ip_addrs"].append(ip_addr)
-
-#Linux	
-#    try:
-#        subprocess.check_output(["ping", "-c", "1", ip_addr])
-#        active_hosts["active_ip_addrs"].append(ip_addr)
-#    except:
-#        inactive_hosts["inactive_ip_addrs"].append(ip_addr)
+	#Check ping result
+	for line in response.readlines():
+		if(line.count("TTL")):
+			active_hosts["active_ip_addrs"].append(ip_addr)
+			exit
+	inactive_hosts["inactive_ip_addrs"].append(ip_addr)
 
 if __name__ == "__main__":
-		
+
+	# Verification of the command line parameters 		
 	if len(sys.argv) < 1:
 		network = ipaddress.ip_network(input("Enter the network to scan : "))
-	
 	if sys.argv[1] == "--network":
 		network = ipaddress.ip_network(sys.argv[2])
-		
-	hosts = network.hosts()
-	
-	executor = concurrent.futures.ThreadPoolExecutor(254)
-	ping_hosts = [executor.submit(pingda, str(ip)) for ip in hosts]
 
-	#this line was added to wait until all thread to be completed
+	# convert network to list of IPs		
+	hosts = network.hosts()
+
+	# Verification of Operation System
+	oper = platform.system()
+	# Based on OS, different Ping Command need to be used
+	if (oper == "Windows"):
+		cmdPing = "ping -n 1 -w 1000"
+	elif (oper == "Linux"):
+		cmdPing = "ping -c 1 "
+	else :
+		cmdPing = "ping -c 1 "
+
+	# Create multiple thread for concurrent ping test	
+	executor = concurrent.futures.ThreadPoolExecutor(254)
+	ping_hosts = [executor.submit(pingsweep, cmdPing, str(ip)) for ip in hosts]
+
+	#To wait until all thread to be completed
 	executor.shutdown(wait=True, cancel_futures=True)
 
+	#Combine all resutls to JSON output
 	json_active_hosts = json.dumps(active_hosts, indent=1)
 	json_inactive_hosts = json.dumps(inactive_hosts, indent=1)
 
+	#Check the command line output whether is there any filter
+	#If there is no filter default is show both active and inactive hosts
 	if len(sys.argv) < 4:
 		print(json_active_hosts, json_inactive_hosts)
 	else:
